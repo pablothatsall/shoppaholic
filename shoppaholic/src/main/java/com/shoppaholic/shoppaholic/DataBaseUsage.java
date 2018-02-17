920faf2d7f62f05ab22ca051d0356f59b4a151dc
RubenIgle <chubioficial@gmail.com>
2018-02-17 14:09:33 +0100

    Add files via upload

--- shoppaholic/src/main/java/com/shoppaholic/shoppaholic/DataBaseUsage.java
+++ shoppaholic/src/main/java/com/shoppaholic/shoppaholic/DataBaseUsage.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class DataBaseUsage implements CommandLineRunner {
  @Autowired
- private ProductRepository repository;
+ private ProductRepository productRepository;
  @Override
  public void run(String... args) throws Exception {
- repository.save(new Product("FIFA", "45", "El mejor simulador de fútbol","Videojuegos","18/10/2017","https://images-eu.ssl-images-amazon.com/images/I/51D3CEXmKjL._SL500_AC_SS250_.jpg"));
- repository.save(new Product("PUBG", "30", "Sobrevive ","2/12/2017","https://images-na.ssl-images-amazon.com/images/I/51P14ZPWsoL._AC_UL160_.jpg"));
+ productRepository.save(new Product("FIFA", 45, "El mejor simulador de fútbol","Videojuegos","18/10/2017","https://images-eu.ssl-images-amazon.com/images/I/51D3CEXmKjL._SL500_AC_SS250_.jpg"));
+ productRepository.save(new Product("PUBG", 30, "Sobrevive ","2/12/2017","https://images-na.ssl-images-amazon.com/images/I/51P14ZPWsoL._AC_UL160_.jpg"));
  }
 }
