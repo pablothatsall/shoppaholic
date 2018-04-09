d5c92f3bb6734f643a599bbbc1aa7abc6674144a
LeiHan1 <32260064+LeiHan1@users.noreply.github.com>
2018-04-09 12:21:06 +0200

    Revert "creating components"
    This reverts commit cc4a9974fb17107850211d14e354c113bdb7497b.

--- shoppaholic/Docker/create_image.sh
+++ shoppaholic/Docker/create_image.sh
@@ -1,2 +1,9 @@
-#!/bin/bash 
-docker build -t pmorenoyurjc/shoppaholic . 
+
+docker run -it --rm --name shoppaholic -v C:/Users/Dani/Downloads/shoppaholic-master/shoppaholic:/usr/src/mymaven -w /usr/src/mymaven maven mvn package
+
+sleep 2
+echo Compress finished, building image 
+sleep 2
+
+mv C:/Users/Dani/Downloads/shoppaholic-master/shoppaholic/Docker/shoppaholic.jar C:/Users/Dani/Downloads/shoppaholic-master/shoppaholic/Docker
+docker build -t shoppaholic C:/Users/Dani/Downloads/shoppaholic-master/shoppaholic/Docker.
