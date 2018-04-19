cbd61de63b758631e3027c722561100ce28086b2
RubenIgle <chubioficial@gmail.com>
2018-04-19 21:17:03 +0200

    Working on the login

--- shoppaholic-angular/src/app/customer.service.ts
+++ shoppaholic-angular/src/app/customer.service.ts
@@ -1,33 +1,52 @@
 
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import {Customer} from './customer.model';
 import 'rxjs/Rx';
 import { Observable } from "rxjs/Observable";
-import { Pedido } from './pedido.model';
-import { Customer } from './customer.model';
 
-
+const URL = '/api';
 
 
 @Injectable()
 export class CustomerService {
 
-
     isLogged = false;
     isAdmin = false;
     customer: Customer;
 
-   constructor(private http: Http) { };
-
-      getCustomer(id:number){
-      	 return this.http.get("http://localhost:4200/api/customer/"+id, { withCredentials: true })
-      .map(response => response.json())
-      .catch(error => this.handleError(error));
-      
+    constructor(private http: Http,private router: Router) {
+        //this.reqIsLogged();
     }
 
+    reqIsLogged() {
 
-  logIn(email: string, pass: string) {
+        const headers = new Headers({
+            'X-Requested-With': 'XMLHttpRequest',
+            //'Access-Control-Allow-Origin': '*'
+        });
+
+        const options = new RequestOptions({ withCredentials: true});
+
+        this.http.get(URL + '/logIn',options).subscribe(
+            response => this.processLogInResponse(response),
+            error => {
+                if (error.status !== 401) {
+                    console.error('Error when asking if logged: ' +
+                        JSON.stringify(error));
+                }
+            }
+        );
+    }
+
+    private processLogInResponse(response) {
+        this.isLogged = true;
+        this.customer = response.json();
+        this.isAdmin = this.customer.roles.indexOf('ROLE_ADMIN') !== -1;
+    }
+
+    logIn(email: string, pass: string) {
         
         const userPass = email + ':' + pass;
 
@@ -35,14 +54,19 @@ export class CustomerService {
             //'Access-Control-Allow-Origin': '*',
             'Authorization': 'Basic ' + utf8_to_b64(userPass),
             'X-Requested-With': 'XMLHttpRequest'
-        });}
+        });
 
-   private handleError(error: any) {
-        console.error(error);
-        return Observable.throw('Server error (' + error.status + '): ' + error.text);
+        const options = new RequestOptions({ withCredentials: true, headers });
+
+        return this.http.get(URL + '/logIn', options).map(
+            response => {
+                this.processLogInResponse(response);
+                return this.customer;
+            }
+        );
     }
 
-logOut() {
+    logOut() {
         //this.reqIsLogged();
         //return this.http.get('https://localhost:8443/api' + '/logOut', { withCredentials: true}).map(
             return this.http.get(URL + '/logOut', { withCredentials: true}).map(
@@ -54,11 +78,27 @@ logOut() {
             }
         );
     }
+      getCustomer(id:number){
+    let url="http://localhost:4200/api/customer/"+id;
+    console.log(url);
+    
+    return this.http.get(url).map(
+      response => response.json())
+      .catch(error =>this.handleError(error))
+  }
+
+  private handleError(error: any) {
+        console.error(error);
+        return Observable.throw('Server error (' + error.status + '): ' + error.text);
+    }
+
+
+
 }
+
 
 function utf8_to_b64(str) {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
         return String.fromCharCode(<any>'0x' + p1);
     }));
-
 }
