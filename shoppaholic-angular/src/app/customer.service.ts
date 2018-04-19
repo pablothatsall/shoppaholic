cd81c40fe18360d9eca443b922ab907afad9fd71
RubenIgle <chubioficial@gmail.com>
2018-04-19 20:33:57 +0200

    Working on come components and imgs

--- shoppaholic-angular/src/app/customer.service.ts
+++ shoppaholic-angular/src/app/customer.service.ts
@@ -1,19 +1,64 @@
 
 import { Injectable } from '@angular/core';
-import { Http, Response,RequestOptions,Headers } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import 'rxjs/Rx';
 import { Observable } from "rxjs/Observable";
 import { Pedido } from './pedido.model';
+import { Customer } from './customer.model';
+
 
 
 
 @Injectable()
 export class CustomerService {
+
+
+    isLogged = false;
+    isAdmin = false;
+    customer: Customer;
+
    constructor(private http: Http) { };
 
       getCustomer(id:number){
-      let url="http://localhost:4200/api/customer/"+id;
+      	 return this.http.get("http://localhost:4200/api/customer/"+id, { withCredentials: true })
+      .map(response => response.json())
+      .catch(error => this.handleError(error));
+      
     }
 
+
+  logIn(email: string, pass: string) {
+        
+        const userPass = email + ':' + pass;
+
+        const headers = new Headers({
+            //'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Basic ' + utf8_to_b64(userPass),
+            'X-Requested-With': 'XMLHttpRequest'
+        });}
+
+   private handleError(error: any) {
+        console.error(error);
+        return Observable.throw('Server error (' + error.status + '): ' + error.text);
+    }
+
+logOut() {
+        //this.reqIsLogged();
+        //return this.http.get('https://localhost:8443/api' + '/logOut', { withCredentials: true}).map(
+            return this.http.get(URL + '/logOut', { withCredentials: true}).map(
+            response => {
+                this.isLogged = false;
+                this.isAdmin = false;
+                this.customer=null;
+                return response;
+            }
+        );
+    }
+}
+
+function utf8_to_b64(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+        return String.fromCharCode(<any>'0x' + p1);
+    }));
 
 }
--- shoppaholic-angular/src/app/login.component.ts
+++ shoppaholic-angular/src/app/login.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
-
+import {CustomerService} from './customer.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
     selector: 'login',
     templateUrl: './login.component.html'
   })
   export class LoginComponent {
+  	 constructor(private router: Router, activatedRoute: ActivatedRoute, private customerService: CustomerService) { }
+
+logIn(event: any, email: string, pass: string) {
+/*
+    event.preventDefault();
+
+    this.customerService.logIn(email, pass).subscribe(
+      u =>{ console.log(u),
+      this.navigate();
+      },
+      //error => alert('Invalid user or password')
+      error=>{console.log(error),
+      alert('Invalid user or password')}
+    );}
+
+      navigate(){
+     this.router.navigate(['/']);
+     */
+  }
+
 
 }
