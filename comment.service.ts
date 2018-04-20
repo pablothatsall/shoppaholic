8fcfe87bc2f1a76c6c37544ebd044f39a3f6df34
sarry150 <sergiosarriaporro@gmail.com>
2018-04-20 19:58:25 +0200

    Add files via upload

---
+++ comment.service.ts
@@ -0,0 +1,18 @@
+
+import { Injectable } from '@angular/core';
+import { Http, Response,RequestOptions,Headers } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from "rxjs/Observable";
+
+
+
+@Injectable()
+export class CommentService {
+   constructor(private http: Http) { };
+
+      getComments(){
+      let url="http://localhost:4200/api/comments/id";
+    }
+
+
+}
---
+++ product.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './product.service';
+import {Product} from './product.model';
+import { Http, Response,Headers, RequestOptions } from '@angular/http';
+const BASE_URL = 'http://localhost:4200/api/product/1';
+const COMMENTS_URL = 'http://localhost:4200/api/comments/1';
+@Component({
+   
+    
+    templateUrl: './product.component.html'
+  })
+  export class ProductComponent {
+    ;
+  product:Product;
+  comments:Comment[];
+   constructor(private router: Router, activatedRoute: ActivatedRoute, private http: Http,  private productService: ProductService){
+       let id = activatedRoute.params.subscribe(params => {
+              
+      this.productService.getProduct(params['id']).subscribe(
+      product =>{
+        this.product=product,
+        this.comments=product.comments;
+      
+      },
+      error =>  console.error(error)
+      );
+     
+      
+  });
+
+  }
+
+  getComments(id:number){
+  let url="http://localhost:4200/api/comments/id";
+    this.commentService.getComments(id).subscribe(
+      comments => { this.comments=product.comments;},
+      error => console.error(error)
+      )
+  }
+
+  getProduct(id:number){
+    this.productService.getProduct(id).subscribe(
+      product =>{ this.product=product;},
+      error => console.error(error)
+    )
+  }
+
+   createProduct(artist:Product){
+    let url="http://localhost:4200/api/admin/newproduct";
+    const options = new RequestOptions({ withCredentials: true});
+    return this.http.post(url,artist,options).map(
+      response => response.json())
+      
+  }
+
+
+  private handleError(error: any) {
+    console.error(error);
+  }
+
+}
