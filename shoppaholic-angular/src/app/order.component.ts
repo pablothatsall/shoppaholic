b70cc1a13a8aefbdf4bf489c41a8ed68735b0f51
DaniRibCha <dani_titihenry@hotmail.com>
2018-04-22 20:50:22 +0200

    Updated order.component.ts but not working

--- shoppaholic-angular/src/app/order.component.ts
+++ shoppaholic-angular/src/app/order.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PedidoService } from './pedido.service';
+import { CustomerService } from './customer.service';
+import {Pedido} from './pedido.model'
+import {Customer} from './customer.model';;
+import { Http, Response,Headers, RequestOptions } from '@angular/http';
 
 @Component({
     selector: 'order',
     templateUrl: './order.component.html'
   })
-  export class OrderComponent {
+  
+export class OrderComponent {
+	order:Pedido;
+    customer:Customer;
 
+ 	constructor(private router: Router, activatedRoute: ActivatedRoute, private http: Http,  private pedidoService: PedidoService, private customerService: CustomerService){
+		let id = activatedRoute.params.subscribe(params => {
+       	this.pedidoService.getPedido(params['id'], customerService.customer).subscribe(
+    order =>{
+        this.order = order
+			},
+			error =>  console.error(error)
+			);
+		});
+	}           
 }
