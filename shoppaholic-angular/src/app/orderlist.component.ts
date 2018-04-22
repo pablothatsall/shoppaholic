e5c5ba5f251e8cf5e9a078697c05d83cf549e90f
DaniRibCha <dani_titihenry@hotmail.com>
2018-04-22 20:48:36 +0200

    Re-factorized orderlist.component.ts

--- shoppaholic-angular/src/app/orderlist.component.ts
+++ shoppaholic-angular/src/app/orderlist.component.ts
@@ -2,31 +2,22 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Http, Response,Headers, RequestOptions } from '@angular/http';
 import { CustomerService } from './customer.service';
-import {Pedido} from './pedido.model'
+import { Pedido } from './pedido.model'
 
 @Component({
     selector: 'orderlist',
     templateUrl: './orderlist.component.html'
-  })
-  export class OrderlistComponent {
-  	orders:Pedido[];
-
-  	 constructor(private router: Router, activatedRoute: ActivatedRoute, private http: Http, private customerService: CustomerService){
-       let id = activatedRoute.params.subscribe(params => {
-       	 this.customerService.getOrders(params['id']).subscribe(
-      orders =>{
-        this.orders=orders;
-     
-        
-      
-      },
-      error =>  console.error(error)
-      );
-     
-      
-  });
-
-  }
-            
-  }
-     
+})
+export class OrderlistComponent {
+ 	orders:Pedido[];
+	constructor(private router: Router, activatedRoute: ActivatedRoute, private http: Http, private customerService: CustomerService){
+		let id = activatedRoute.params.subscribe(params => {
+			this.customerService.getOrders(params['id']).subscribe(
+				orders =>{
+					this.orders=orders;
+				},
+				error =>  console.error(error)
+			);
+		});
+	}
+}
